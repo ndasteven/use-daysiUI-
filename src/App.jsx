13d89105c8abd553e2a12dbc16477b8d2f396224
ndasteven <ndasteven5@gmail.com>
2024-11-21 19:18:58 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import Profile from './Profile';
 
 function App() {
 
+  const films = [{titre:'Spiderman', acteur:'Steven', image:'https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp'},{titre:'Titanic', acteur:'Jack', image:'https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp'}]
 
   return(
   <>
-    <Profile />
+  {
+    films.map((item) => (
+      <Profile titre={item.titre} acteur={item.acteur} image={item.image} />
+    ) )
+}
+    
   </>
   )
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,7 +4,20 @@ export default function Profile(props) {
   return (
     
     <div>
-      
+      <div className="card card-side bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src={props.image}
+      alt="Movie" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.titre}</h2>
+    <p>{props.acteur}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
