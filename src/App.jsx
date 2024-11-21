c41e149191e0998f432ba495a82390ba6b4c7473
ndasteven <ndasteven5@gmail.com>
2024-11-21 19:02:16 +0100

    inititial daysiu

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,13 @@ import { useState } from 'react';
 import Profile from './Profile';
 
 function App() {
-  const [theme, setTheme] = useState('light');
 
-  const toggleTheme = () => {
-    const themes = ['light', 'dark', 'cupcake', 'cyberpunk', 'forest'];
-    const currentIndex = themes.indexOf(theme);
-    const nextIndex = (currentIndex + 1) % themes.length;
-    setTheme(themes[nextIndex]);
-  };
 
-  return;
+  return(
   <>
-    <Profile titre="hello" />
-  </>;
+    <Profile />
+  </>
+  )
 }
 
 export default App;
